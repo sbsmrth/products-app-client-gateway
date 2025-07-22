63848776f671bd9fd3b5366bdcd08ba34396e6a1
sbsmrth <andresf.mrh@gmail.com>
2025-07-22 10:58:01 -0500

    feat: add dotenv and joi for config validation, update app to use env vars and improved logging

---
+++ src/config/envs.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true); // Allow unknown properties
+
+const { error, value } = envsSchema.validate(process.env) as {
+  error: joi.ValidationError;
+  value: EnvVars;
+};
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  port: envVars.PORT,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+
+  logger.log(`Gateway is running on port ${envs.port}`);
 }
 bootstrap();
