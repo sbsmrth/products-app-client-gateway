bfafb31e9d822ef493397ced5f631fd3aa6dbc94
sbsmrth <andresf.mrh@gmail.com>
2025-08-02 16:03:45 -0500

    changes to client gateway from products-ms-app host repository

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
-  logger.log(`Gateway is running on port ${envs.port}`);
+  logger.log(`App Gateway is running on port ${envs.port}`);
 }
 bootstrap();
